94192e230c5a96c69d1427e6b940788a73a96e68
RT <tomi.makinen@hotmail.fi>
2023-01-29 13:00:27 +0200

    Change webserver to ESPAsyncWebServer

--- src/WebSerialStream.cpp
+++ src/WebSerialStream.cpp
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Library that provides a fanout, or T-flow; so that output or logs do 
- * not just got to the serial port; but also to a configurable mix of a 
+ * Library that provides a fanout, or T-flow; so that output or logs do
+ * not just got to the serial port; but also to a configurable mix of a
  * telnetserver, a webserver, syslog or MQTT.
  */
 
@@ -22,53 +22,56 @@
 
 #if (defined(ESP32) || defined(ESP8266))
 
-size_t WebSerialStream::write(uint8_t c) {
+size_t WebSerialStream::write(uint8_t c)
+{
   _buff[_at % sizeof(_buff)] = c;
   _at++;
   return 1;
 }
 
-WebSerialStream::~WebSerialStream() {
+WebSerialStream::~WebSerialStream()
+{
   stop();
 }
 
-void WebSerialStream::begin() {
+void WebSerialStream::begin()
+{
   if (_server != NULL)
     return;
-  _server = new WebServer(_webPort);
-  _server->on("/",[this]() {
-  _server->send(200, "text/html", "<html><head><title>log</title></head>"
-	"<style>"
-		"#log { font-family: 'Courier New', monospace; white-space: pre; }"
-	"</style>"
-	"<script language=javascript>"
-        "var at = 0;"
-	"function st() { setTimeout(750, f()); };"
-	"function f() { fetch('log?at='+at)."
-                "then("
-		   "r => { return r.json(); }"
-                ").then( "
-                   "j => { " 
-			    /* we do rouhgly 'at the end' as Macs and Edge are a few pixels off. */
-			  " isAtEnd = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 4; " 
-                          " document.getElementById('log').innerHTML += j.buff; "
-                          " at= j.at; "
-                          " if (isAtEnd) window.scrollTo(0,document.body.scrollHeight); "
- 			  " st(); "
-                   "}"
-		").catch( e => { console.log(e); st(); } "
-                ");"
-        "};"
-        "window.onLoad = f();"
-	"</script>"
-	"<body><div id=log></div></body></html>");
-  });
-  _server->on("/log",[this]() {
-    if (!_server->hasArg("at")) {
-       _server->send(400, "text/plain", "Missing at argument.");
+  _server = new AsyncWebServer(_webPort);
+  _server->on("/", [this](AsyncWebServerRequest *request)
+              { request->send(200, "text/html", "<html><head><title>log</title></head>"
+                                                "<style>"
+                                                "#log { font-family: 'Courier New', monospace; white-space: pre; }"
+                                                "</style>"
+                                                "<script language=javascript>"
+                                                "var at = 0;"
+                                                "function st() { setTimeout(750, f()); };"
+                                                "function f() { fetch('log?at='+at)."
+                                                "then("
+                                                "r => { return r.json(); }"
+                                                ").then( "
+                                                "j => { "
+                                                /* we do rouhgly 'at the end' as Macs and Edge are a few pixels off. */
+                                                " isAtEnd = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 4; "
+                                                " document.getElementById('log').innerHTML += j.buff; "
+                                                " at= j.at; "
+                                                " if (isAtEnd) window.scrollTo(0,document.body.scrollHeight); "
+                                                " st(); "
+                                                "}"
+                                                ").catch( e => { console.log(e); st(); } "
+                                                ");"
+                                                "};"
+                                                "window.onLoad = f();"
+                                                "</script>"
+                                                "<body><div id=log></div></body></html>"); });
+  _server->on("/log", [this](AsyncWebServerRequest *request)
+              {
+    if (!request->hasArg("at")) {
+       request->send(400, "text/plain", "Missing at argument.");
        return;
     };
-    unsigned long prevAt= _server->arg("at").toInt();
+    unsigned long prevAt= request->arg("at").toInt();
     String out = "{\"at\":" + String(_at) + ",\"buff\":\"";
 
     // reset browsers from the future (e.g. after a reset)
@@ -98,24 +101,23 @@ void WebSerialStream::begin() {
        };
     };
     out += "\"}";
-    _server->send(200, "application/json", out);
-  });
+    request->send(200, "application/json", out); });
   _server->begin();
 
   Log.printf("Opened serial web server on http://%s:%d\n", WiFi.localIP().toString().c_str(), _webPort);
   MDNS.addService("http", "tcp", _webPort);
 };
 
-void WebSerialStream::stop() {
+void WebSerialStream::stop()
+{
   if (!_server)
     return;
-  _server->stop();
+  _server->end();
   delete _server;
   _server = NULL;
 }
 
-void WebSerialStream::loop() {
-  if (_server)
- 	_server->handleClient();
+void WebSerialStream::loop()
+{
 }
 #endif
--- src/WebSerialStream.h
+++ src/WebSerialStream.h
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Library that provides a fanout, or T-flow; so that output or logs do 
- * not just got to the serial port; but also to a configurable mix of a 
+ * Library that provides a fanout, or T-flow; so that output or logs do
+ * not just got to the serial port; but also to a configurable mix of a
  * telnetserver, a webserver, syslog or MQTT.
  */
 
@@ -26,32 +26,32 @@
 #ifdef ESP32
 #include <WiFi.h>
 #include <ESPmDNS.h>
-#include <WebServer.h>
 #endif
 
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-typedef ESP8266WebServer WebServer;
 #endif
 
+#include <ESPAsyncWebServer.h>
 
-class WebSerialStream : public TLog {
-  public:
-    WebSerialStream(const uint16_t webPort = 80) : _webPort(webPort) {};
-    ~WebSerialStream();
-    virtual size_t write(uint8_t c);
-    virtual void begin();
-    virtual void loop();
-    virtual void stop();
-  private:
-    uint16_t _webPort;
-    WebServer * _server = NULL;
-    uint8_t _buff[1024];
-    unsigned long _at = 0;
-  protected:
+class WebSerialStream : public TLog
+{
+public:
+  WebSerialStream(const uint16_t webPort = 80) : _webPort(webPort){};
+  ~WebSerialStream();
+  virtual size_t write(uint8_t c);
+  virtual void begin();
+  virtual void loop();
+  virtual void stop();
+
+private:
+  uint16_t _webPort;
+  AsyncWebServer *_server = NULL;
+  uint8_t _buff[1024];
+  unsigned long _at = 0;
+
+protected:
 };
 #endif
 #endif
-
